9984bd4ed9ecd467ca7336aec0679c66f9f71e42
Renato Orgito <orgito@gmail.com>
2018-03-01 15:53:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# uclcoin-nodes
---
+++ node.py
@@ -0,0 +1,40 @@
+#!python
+
+import re
+
+from flask import Flask, jsonify
+from uclcoin import Block, BlockChain, Transaction
+
+blockchain = BlockChain()
+
+app = Flask(__name__)
+
+
+@app.route('/balance/<address>', methods=['GET'])
+def get_balance(address):
+    if not re.match(r'[\da-f]{66}$', address):
+        return jsonify({'message': 'Invalid address'}), 400
+
+    balance = blockchain.get_balance(address)
+    return jsonify({'balance': balance}), 200
+
+
+@app.route('/block/<index>', methods=['GET'])
+def get_block(index):
+    if index == 'last':
+        block = blockchain.get_latest_block()
+    else:
+        block = blockchain.get_block_by_index(int(index))
+    if not block:
+        return jsonify({'message': 'Block not found'}), 404
+
+    return jsonify(dict(block)), 200
+
+
+@app.route('/block/minable/<address>', methods=['GET'])
+def get_minable_block(address):
+    if not re.match(r'[\da-f]{66}$', address):
+        return jsonify({'message': 'Invalid address'}), 400
+
+    block = blockchain.get_minable_block(address)
+    return jsonify(dict(block)), 200
